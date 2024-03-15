5c4d4ef1d938cb7bb0a69429940c40ee6e8a990f
Dima Rekesh <bmwshop@gmail.com>
2024-03-14 20:32:40 -0700

    pi

--- vllm/model_executor/layers/rotary_embedding.py
+++ vllm/model_executor/layers/rotary_embedding.py
@@ -94,6 +94,9 @@ class RotaryEmbedding(nn.Module):
         """Compute the cos and sin cache."""
         inv_freq = self._compute_inv_freq(self.base)
         t = torch.arange(self.max_position_embeddings, dtype=torch.float)
+        pi_factor = os.getenv('PI_FACTOR')
+        if pi_factor:
+            t *= 1 / pi_factor
 
         freqs = torch.einsum("i,j -> ij", t, inv_freq)
         cos = freqs.cos()
